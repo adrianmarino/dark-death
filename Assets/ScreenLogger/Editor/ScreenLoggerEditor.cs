08d1723256611c280a90cec6cd63fa731ca0c8b3
Adrian Norberto Marino <adrian.marino@almundo.com>
2017-04-02 23:08:22 -0300

    format file

--- Assets/ScreenLogger/Editor/ScreenLoggerEditor.cs
+++ Assets/ScreenLogger/Editor/ScreenLoggerEditor.cs
@@ -4,31 +4,28 @@ using UnityEditor;
 
 namespace AClockworkBerry
 {
-    [CustomEditor(typeof(ScreenLogger))]
-    public class ScreenLoggerEditor : Editor
-    {
-        [MenuItem("GameObject/Create Other/Screen Logger")]
-        static void AddScreenLogger()
-        {
-            if (GameObject.FindObjectOfType<ScreenLogger>() == null)
-            {
-                GameObject gameObject = new GameObject();
-                gameObject.name = "ScreenLogger";
-                gameObject.AddComponent<ScreenLogger>();
-            }
-            else
-            {
-                Debug.LogError("ScreenLogger already added to the scene.");
-            }
-        }
+	[CustomEditor (typeof(ScreenLogger))]
+	public class ScreenLoggerEditor : Editor
+	{
+		[MenuItem ("GameObject/Create Other/Screen Logger")]
+		static void AddScreenLogger ()
+		{
+			if (GameObject.FindObjectOfType<ScreenLogger> () == null) {
+				GameObject gameObject = new GameObject ();
+				gameObject.name = "ScreenLogger";
+				gameObject.AddComponent<ScreenLogger> ();
+			} else {
+				Debug.LogError ("ScreenLogger already added to the scene.");
+			}
+		}
 
-        public override void OnInspectorGUI()
-        {
-            base.DrawDefaultInspector();
+		public override void OnInspectorGUI ()
+		{
+			base.DrawDefaultInspector ();
 
-            ScreenLogger.Instance.InspectorGUIUpdated();
-        }
-    }
+			ScreenLogger.Instance.InspectorGUIUpdated ();
+		}
+	}
 }
 
 /*
