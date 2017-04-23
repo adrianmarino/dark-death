0e22580ba6b7544b956fc9543b5b9fb614e22bec
Adrian Norberto Marino <adrian.marino@almundo.com>
2017-04-23 11:31:08 -0300

    recoil script

---
+++ Assets/Scripts/Player/WeaponShootAnimator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class WeaponShootAnimator : MonoBehaviour
+{
+	//-----------------------------------------------------------------------------
+	// Events
+	//-----------------------------------------------------------------------------
+
+	void Update ()
+	{
+		if (Util.Input.GetFireButton ())
+			Animate ();
+	}
+
+	//-----------------------------------------------------------------------------
+	// Private Methods
+	//-----------------------------------------------------------------------------
+
+	void Animate ()
+	{
+		transform.position = new Vector3 (
+			transform.position.x,
+			transform.position.y,
+			15f
+		);
+	}
+
+	//-----------------------------------------------------------------------------
+	// Attributes
+	//-----------------------------------------------------------------------------
+
+	// The minimum vibration power
+	[SerializeField]
+	private float minVibration = 15f;
+
+	// The maximum vibration power
+	[SerializeField]
+	private float maxVibration = 15f;
+
+	private bool firing;
+}
+
+
