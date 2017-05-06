49fa0a407b8cc71d45861cf027c5922be2bc3c52
Adrian Norberto Marino <adrian.marino@almundo.com>
2017-05-06 12:45:33 -0300

    adjust weapons muzzle flash

--- Assets/Scripts/Util/CollectionExtensions.cs
+++ Assets/Scripts/Util/CollectionExtensions.cs
@@ -7,14 +7,12 @@ namespace Util
 		public static T Next<T> (this IList<T> list, T item)
 		{
 			int index = (list.IndexOf (item) + 1) == list.Count ? 0 : (list.IndexOf (item) + 1);
-			UnityEngine.Debug.Log ("index: " + index);
 			return list [index];
 		}
 
 		public static T Previous<T> (this IList<T> list, T item)
 		{
 			int index = (list.IndexOf (item) - 1) < 0 ? list.Count - 1 : (list.IndexOf (item) - 1);
-			UnityEngine.Debug.Log ("index: " + index);
 			return list [index];
 		}
 	}
