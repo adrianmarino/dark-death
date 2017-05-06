c268965866fa30769fad2f29e934039854954655
Adrian Norberto Marino <adrian.marino@almundo.com>
2017-05-06 12:26:25 -0300

    create pistol and sniper weapons

---
+++ Assets/Scripts/Weapon/Impl/Pistol.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fps.Weapon.Animation;
+
+namespace Fps.Weapon
+{
+	[RequireComponent (typeof(WeaponRecoilAnimation))]
+	[RequireComponent (typeof(WeaponSwayAnimation))]
+	[RequireComponent (typeof(WeaponReloadAnimation))]
+	public class Pistol : RechargeableWeapon
+	{
+		//-----------------------------------------------------------------------------
+		// Public Methods
+		//-----------------------------------------------------------------------------
+
+		public override void HitTargetAction (Vector3 position, Vector3 normal)
+		{
+			GameObject _hitEffect = Instantiate (
+				                        HitEffect,
+				                        position,
+				                        Quaternion.LookRotation (normal)
+			                        );
+			Destroy (_hitEffect, 2f);
+		}
+
+		public override void PlayShootEffectAction ()
+		{
+			muzzleFlash.Play ();
+			ShootSound.Play ();
+			shootSmoke.Play ();
+			WeaponRecoilAnimation ().Play ();
+		}
+
+		public override void PlayReloadEffectAction ()
+		{
+			WeaponReloadAnimation ().Play ();
+			ReloadSound.Play ();
+		}
+
+		//-----------------------------------------------------------------------------
+		// Properties
+		//-----------------------------------------------------------------------------
+
+		AudioSource ShootSound {
+			get { return GetComponents<AudioSource> () [0]; }
+		}
+
+		AudioSource ReloadSound {
+			get { return GetComponents<AudioSource> () [1]; }
+		}
+
+		WeaponRecoilAnimation WeaponRecoilAnimation ()
+		{
+			return GetComponent<WeaponRecoilAnimation> ();
+		}
+
+		WeaponReloadAnimation WeaponReloadAnimation ()
+		{
+			return GetComponent<WeaponReloadAnimation> ();
+		}
+
+		GameObject HitEffect {
+			get { return hitEffect; }
+		}
+
+		//-----------------------------------------------------------------------------
+		// Attributes
+		//-----------------------------------------------------------------------------
+
+		[SerializeField]
+		private ParticleSystem muzzleFlash;
+
+		[SerializeField]
+		private ParticleSystem shootSmoke;
+
+		[SerializeField]
+		private GameObject hitEffect;
+
+		//-----------------------------------------------------------------------------
+		// Constructors
+		//-----------------------------------------------------------------------------
+
+		private Pistol () : base ()
+		{
+		}
+	}
+}
