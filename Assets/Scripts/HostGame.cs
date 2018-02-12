0fcc94d983ca2f202d3b0c5c79950e6a9837bb22
Adrian Marino <adrian.marino@restorando.com>
2018-02-12 11:57:51 -0300

    host game script

---
+++ Assets/Scripts/HostGame.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+    void Start()
+    {
+        _networkManager = NetworkManager.singleton;   
+        if (_networkManager.matchMaker == null)
+            _networkManager.StartMatchMaker();
+    }
+   
+    public void CreateRoom()
+    {
+        if (String.IsNullOrEmpty(_roomName) && String.IsNullOrEmpty(_roomPassword) && _roomSize >= 2) return;
+
+        Debug.LogFormat("Create {0} room for {1} player.");
+        _networkManager.matchMaker.CreateMatch(
+            _roomName, 
+            _roomSize, 
+            true, 
+            _roomPassword,
+            "",
+            "",
+            0,
+            0,
+            _networkManager.OnMatchCreate
+        );
+    }
+
+    #region Attributes
+
+    [SerializeField]
+    private uint _roomSize = 6;
+
+    [SerializeField]
+    private string _roomName;
+    
+    [SerializeField]
+    private string _roomPassword;
+
+    private NetworkManager _networkManager;
+
+    #endregion
+}
+
