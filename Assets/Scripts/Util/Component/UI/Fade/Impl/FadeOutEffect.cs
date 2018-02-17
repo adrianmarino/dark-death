db928402c7bbb73d5d9b10f5640e3df1444062d3
Adrian Marino <adrian.marino@restorando.com>
2018-02-17 11:36:46 -0300

    Scene transacion refactor. Simplify Fade effect abstraction

---
+++ Assets/Scripts/Util/Component/UI/Fade/Impl/FadeOutEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Util.Component.UI.Fade
+{
+    public class FadeInEffect : FadeEffect
+    {        
+        public override bool doesFinish()
+        {
+            return alpha == 1;
+        }
+
+        protected FadeInEffect(Texture2D fadeOutTexture, float speed, int drawDepth) : base(fadeOutTexture, speed, drawDepth)
+        {
+            direction = 1;
+            alpha = 0;
+        }
+    }
+}
